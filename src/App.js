062692714ae30f6b79ceb37551aef57f284907a9
sahil2311sharma <sahilsharma123027@gmail.com>
2022-09-29 00:37:45 +0530

    more on states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
   const expenses = [
     {
@@ -35,6 +35,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,23 @@
-import React, { useState } from 'react';
+import React,{ useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
   const [title,setTitle] = useState(props.title);
-  
-  const changeTitle = () => {
-    setTitle("New Title");
+  const title_Change_Listener = () => {
+    setTitle('Changed');
     console.log(title);
   }
-
-  return (
+    return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={changeTitle}>Change Title</button>
+      <button onClick={title_Change_Listener} >Change Title</button>
     </Card>
   );
 }
